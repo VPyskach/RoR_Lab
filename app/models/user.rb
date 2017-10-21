49b95f33e457005f65a440456b4ae6cbb78c74ca
vasya <v.pyskach@gmail.com>
2017-10-21 14:07:26 +0300

    new commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
 
+	validates :firstname, :address, :lastname, :email, :password, presence: true
+	validates :email, uniqueness: true
+	validates :password, length: { in: 3..6 }
+	validates :email, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/,
+	    message: "Check yours email" }
 end
